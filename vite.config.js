d5738a74ed9b86c5699d77a1906a962f06bc2bfe
kenbunroku <kenbunroku524@gmail.com>
2023-11-18 14:25:32 -0600

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+
+export default defineConfig(() => {
+  return {
+    root: "./",
+    publicDir: "../public",
+    base: "./",
+    plugins: [glsl()],
+    server: {
+      host: true,
+    },
+  };
+});
